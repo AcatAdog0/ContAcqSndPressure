023e3fa693e4b956cbaa221e0818c7364d9a9c25
Yanldt <imyanl.dt@gmail.com>
2020-12-18 14:46:31 +0800

    添加项目文件。

---
+++ ContAcqSndPressure/ContAcqSndPressure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContAcqSndPressure
+{
+    public partial class ContAcqSndPressure : Form
+    {
+        public ContAcqSndPressure()
+        {
+            InitializeComponent();
+        }
+    }
+}
